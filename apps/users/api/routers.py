71cb2a76e920782945ea8cab4ff3c835b5926808
Jorge Quintui <jorgealequinn@gmail.com>
2022-08-14 19:01:03 -0400

    Update: Se actualiza el modulo de rutas de la app usuarios

--- apps/users/api/routers.py
+++ apps/users/api/routers.py
@@ -1,9 +1,9 @@
 from rest_framework.routers import DefaultRouter
 
-from apps.users.api.views.users_views import UserViewSet
+from apps.users.api.client.views.clients_views import ClientViewSet
 
 router = DefaultRouter()
 
-router.register('',UserViewSet, basename='users')
+router.register('client',ClientViewSet, basename='client')
 
 urlpatterns = router.urls
