079221e26c3c908d04f0c201946c592d2086f2e4
Jorge Quintui <jorgealequinn@gmail.com>
2022-08-14 18:55:34 -0400

    Create: se crea modulo de modelos para serializar en la app cliente

---
+++ apps/users/api/client/models/db_models.py
@@ -0,0 +1,1 @@
+# Modelos para acceder mediante ORM de django
---
+++ apps/users/api/client/models/models.py
@@ -0,0 +1,6 @@
+
+# Modelos para hacer querys con procs
+
+class Client(object):
+    def __init__(self, resultado : str):
+        self.resultado = resultado
