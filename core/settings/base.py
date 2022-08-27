41bf99c6c1646830c7980a9e84cc2df94dad2498
Jorge Quintui <jorgealequinn@gmail.com>
2022-08-26 21:33:57 -0400

    Update: Se configura la ruta de los archivos estáticos

--- core/settings/base.py
+++ core/settings/base.py
@@ -141,9 +141,8 @@ CORS_ALLOW_CREDENTIALS = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-#STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, '../static'),)
 
 EMAIL_HOST = env.str('EMAIL_HOST')
 EMAIL_PORT = env.str('EMAIL_PORT')
