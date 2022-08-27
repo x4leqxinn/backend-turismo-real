c57f1360974c8b58711aa9980e49ac6d1704df5c
Jorge Quintui <jorgealequinn@gmail.com>
2022-08-26 21:32:57 -0400

    Update: Se agrega al panel de admin Paises, Estados y Ciudades

--- apps/locations/admin.py
+++ apps/locations/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from apps.locations.models import *
 
 # Register your models here.
+
+admin.site.register(Pais)
+admin.site.register(EstadoPais)
+admin.site.register(Ciudad)
+
