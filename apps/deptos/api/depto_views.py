8db4d88bc310da3da23c37ae2a1e976f993c0cc9
Jorge Quintui <jorgealequinn@gmail.com>
2022-09-23 18:14:45 -0300

    Update: Se agregan vistas
    Se agrega la vista de comentario, Galería de interiores y Galería de exteriores

--- apps/deptos/api/depto_views.py
+++ apps/deptos/api/depto_views.py
@@ -22,3 +22,30 @@ class DeptoRetrieveAPIView(generics.RetrieveAPIView):
     def get_queryset(self):
         return self.get_serializer().Meta.model.objects.filter(estado = 'ACTIVO')
         
+class InteriorGaleryListAPIView(generics.ListAPIView):
+    serializer_class = InteriorGalerySerializer
+    filterset_class  = InteriorFilter
+    search_fields = ['id_viv__id']
+    ordering_fields = ['id','id_viv__id']
+    ordering = ['id','id_viv__id']
+
+    def get_queryset(self):
+        return  GaleriaInterior.objects.filter(estado = 'ACTIVO')
+class ExteriorGaleryListAPIView(generics.ListAPIView):
+    serializer_class = ExteriorGalerySerializer
+    filterset_class  = ExteriorFilter
+    search_fields = ['id_viv__id']
+    ordering_fields = ['id','id_viv__id']
+    ordering = ['id','id_viv__id']
+
+    def get_queryset(self):
+        return  GaleriaExterior.objects.filter(estado = 'ACTIVO')
+class CommentListAPIView(generics.ListAPIView):
+    serializer_class = CommentSerializer
+    filterset_class  = CommentFilter
+    search_fields = ['id_cli__id_viv__id']
+    ordering_fields = ['id','id_cli__id_cli__id__id','id_cli__id_viv__id']
+    ordering = ['id','id_cli__id_viv__id']
+
+    def get_queryset(self):
+        return Comentario.objects.filter(estado = 'ACTIVO')
