602e45c3ef4862e102fb4d6f63934a4bbca1876f
Jorge Quintui <jorgealequinn@gmail.com>
2022-09-26 03:49:37 -0300

    Update: SP CREATE CLIENTE

--- apps/users/api/client/connections/clients_sp.py
+++ apps/users/api/client/connections/clients_sp.py
@@ -1,6 +1,8 @@
 # Importamos la librería para conectarnos a la base de datos
 from django.db import connections
 
+from apps.users.api.client.models.models import Client
+
 def listClient(db_user):
     # La sentencia with se encarga de cerrar la conexión de forma implicita
     with connections[db_user].cursor() as cursor:
@@ -19,12 +21,48 @@ def retrieveClient(db_user,pk):
     pass
 
 
-def createClient(db_user,client):
+def createClient(db_user,client : Client):
+    from datetime import datetime
     # La sentencia with se encarga de cerrar la conexión de forma implicita
     with connections[db_user].cursor() as cursor:
         
         # CONSUMIR VARIABLES
         outval = cursor.var(int).var
-    
+        #('42534543','2',null,'Jorge','Alejandro','Quintui','Vergara',to_date('02/10/2000'),'65928439','1384','Monas Xinas Crew',1,1,1,1,1,2,:b_out_num)
+        # llamado
+        cursor.callproc("PKG_CLIENT.SP_CREATE_CLIENT",[
+            client.rut,
+            client.dv,
+            client.pasaporte,
+            client.nombre,
+            client.snombre,
+            client.ap_paterno,
+            client.ap_materno,
+            datetime.strptime(client.fecha_nacimiento, '%d/%m/%Y'),
+            client.telefono,
+            client.num_calle,
+            client.calle,
+            client.id_ciu,
+            client.id_est,
+            client.id_pai,
+            client.id_doc,
+            client.id_est_civ,
+            client.id_gen,
+            outval
+        ])
         print(outval.getvalue())        
-    pass
+    return outval.getvalue()
+
+
+
+
+'''
+
+-- EXECUTE PKG_CLIENT.SP_CREATE_CLIENT('42534543','2',null,'Jorge','Alejandro','Quintui','Vergara',to_date('02/10/2000'),'65928439','1384','Monas Xinas Crew',1,1,1,1,1,2,:b_out_num);
+
+
+-- EXECUTE PKG_CLIENT.SP_UPDATE_CLIENT(1,'20281676','2',null,'Jorge','Alejandro','Quintui','Vergara',SYSDATE,'65928439','1384','Monas Xinas Crew',1,1,1,1,1,2,:b_out_num);
+
+-- EXECUTE PKG_CLIENT.SP_DELETE_CLIENT(1,:b_out_num);
+
+'''
