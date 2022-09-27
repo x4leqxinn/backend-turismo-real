bb3c45bb65f3ecdd926079eb51c117165511c8b2
Joaquín Reyes <joaking.twitch3@gmail.com>
2022-09-27 00:40:11 -0400

    CREATE: routers.py

--- apps/employees/api/routers.py
+++ apps/employees/api/routers.py
@@ -1,10 +1,12 @@
 from rest_framework.routers import DefaultRouter
 
 from apps.employees.api.position.views import PositionViewSet
+from apps.employees.api.employee.views import EmployeeViewSet
 
 router = DefaultRouter()
 
 router.register('position',PositionViewSet, basename='position')
+router.register('employee',EmployeeViewSet, basename='employee')
 
 urlpatterns = router.urls
 
