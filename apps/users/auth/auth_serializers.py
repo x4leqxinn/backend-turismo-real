7012b94550bf65a0bf80583f5c3ce5d3d9cf8139
Jorge Quintui <jorgealequinn@gmail.com>
2022-09-28 02:17:55 -0300

    Update: Cargo empleado

--- apps/users/auth/auth_serializers.py
+++ apps/users/auth/auth_serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from apps.base.models.db_models import Empleado
 from apps.users.models import User
 
 # Serializer para login 
@@ -6,17 +7,25 @@ class UserTokenSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = User
-        fields = ('email', 'first_name', 'last_name', 'role', 'image')
+        fields = ('email', 'first_name', 'last_name', 'role', 'image','person')
     
     # Se define la data a mostrar al iniciar sesión
     def to_representation(self, instance):
-        return {
-            'email' : instance.email,
-            'role' : {
-                'id' : instance.role.id,
-                'description' : instance.role.description
-            },
-            'image' : instance.image.url if instance.image != '' else ''
-        }
+        data = { 'email' : instance.email,
+                'role' : {
+                    'id' : instance.role.id,
+                    'description' : instance.role.description
+                },
+                'image' : instance.image.url if instance.image != '' else '',
+                'person' : instance.person.id
+            }
+        # TODO: Se debe indicar el id del ROLE ASIGNADO PARA LOS EMPLEADOS
+        if instance.role.id == 21:
+            empleado = Empleado.objects.get(id = instance.person.id)
+            data['cargo'] = {
+                'id' : empleado.id_car.id,
+                'cargo' : empleado.id_car.descripcion
+            }
+        return data
 
 
