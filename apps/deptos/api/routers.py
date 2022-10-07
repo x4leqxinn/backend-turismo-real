656a339fda8bab810da75a9ffebb42cf7804772d
Jorge Quintui <jorgealequinn@gmail.com>
2022-10-07 20:11:03 -0300

    Delete: Depto Module Deleted :innocent:

--- apps/deptos/api/routers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.routers import DefaultRouter
-
-from apps.deptos.api.depto_views import InventoryViewSet
-
-router = DefaultRouter()
-
-router.register('inventory',InventoryViewSet, basename='inventory')
-
-urlpatterns = router.urls
