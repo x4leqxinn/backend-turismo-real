b41899383d5972cadd184d65f911dcd53a48119b
Jorge Quintui <jorgealequinn@gmail.com>
2022-10-08 15:25:18 -0300

    Update: New Custom Admin For Sala and Cargo

--- apps/base/admin.py
+++ apps/base/admin.py
@@ -90,9 +90,90 @@ class GeneroAdmin(admin.ModelAdmin):
 
 admin.site.register(Genero,GeneroAdmin)
 
-# Register your models here.
-admin.site.register(Sala)
-admin.site.register(Cargo)
+
+# Sala Admin
+class SalaAdminForm(forms.ModelForm):
+    estado = ChoiceField(choices=STATE_CHOICES)
+    
+    class Meta:
+        model = Sala
+        fields = ('descripcion','estado',)
+class SalaAdmin(admin.ModelAdmin):
+    actions = ['active_state','inactive_state']
+    list_display = ('id','descripcion','estado')
+    ordering = ('id', 'descripcion')
+    search_fields = ('descripcion', 'id','estado')
+    list_editable = ('descripcion',)
+    list_display_links = ('id',)
+    list_filter= ('descripcion','estado') 
+    list_per_page = 5
+    form = SalaAdminForm
+
+    @admin.action(description='Cambiar estado ACTIVO')
+    def active_state(self,request,queryset):
+        for sala in queryset:
+            sala.estado = 'ACTIVO'
+            sala.save()
+    
+    @admin.action(description='Cambiar estado INACTIVO')
+    def inactive_state(self,request,queryset):
+        for sala in queryset:
+            sala.estado = 'INACTIVO'
+            sala.save()
+
+    def get_actions(self, request):
+        actions = super().get_actions(request)
+        if 'delete_selected' in actions:
+            del actions['delete_selected']
+        return actions
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+admin.site.register(Sala,SalaAdmin)
+
+# Cargo Admin
+class CargoAdminForm(forms.ModelForm):
+    estado = ChoiceField(choices=STATE_CHOICES)
+    
+    class Meta:
+        model = Cargo
+        fields = ('descripcion','estado',)
+
+class CargoAdmin(admin.ModelAdmin):
+    actions = ['active_state','inactive_state']
+    list_display = ('id','descripcion','estado')
+    ordering = ('id', 'descripcion')
+    search_fields = ('descripcion', 'id','estado')
+    list_editable = ('descripcion',)
+    list_display_links = ('id',)
+    list_filter= ('descripcion','estado') 
+    list_per_page = 5
+    form = CargoAdminForm
+
+    @admin.action(description='Cambiar estado ACTIVO')
+    def active_state(self,request,queryset):
+        for cargo in queryset:
+            cargo.estado = 'ACTIVO'
+            cargo.save()
+    
+    @admin.action(description='Cambiar estado INACTIVO')
+    def inactive_state(self,request,queryset):
+        for cargo in queryset:
+            cargo.estado = 'INACTIVO'
+            cargo.save()
+
+    def get_actions(self, request):
+        actions = super().get_actions(request)
+        if 'delete_selected' in actions:
+            del actions['delete_selected']
+        return actions
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+admin.site.register(Cargo,CargoAdmin)
+
 admin.site.register(Color)
 admin.site.register(Modelo)
 admin.site.register(Disponibilidad)
