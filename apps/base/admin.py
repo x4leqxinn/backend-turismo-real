06a409351578effdf20a64627c13e8ad1fd079a9
Jorge Quintui <jorgealequinn@gmail.com>
2022-10-08 15:17:23 -0300

    Update: New Custom Admin For Genero

--- apps/base/admin.py
+++ apps/base/admin.py
@@ -1,68 +1,100 @@
 from django.contrib import admin
 from apps.base.models.db_models import *
 from django import forms
-
-'''
-class SelectWOA(Select):
-    def create_option(self, name, value, label, selected, index, 
-                      subindex=None, attrs=None):
-        option_dict = super(SelectWOA, self).create_option(name, value, 
-            label, selected, index, subindex=subindex, attrs=attrs)
-            #Category.objects.
-        try:
-            option_dict['attrs']['style'] =  'color: ' +  Category.objects.get(name=label).color + ';' 
-        except:
-            pass
-            
-        return option_dict
-
-from django.forms import ChoiceField, ModelChoiceField
-from category.models import Category
-
-'''
-
-from django.forms import ChoiceField, ModelChoiceField
+from django.forms import ChoiceField
 
 STATE_CHOICES = (
     ("ACTIVO", "ACTIVO"),
     ("INACTIVO", "INACTIVO"),
 )
 
-
+# Marca Admin
 class MarcaAdminForm(forms.ModelForm):
     estado = ChoiceField(choices=STATE_CHOICES)
     
     class Meta:
         model = Marca
         fields = ('nombre','estado',)
-
-
 class MarcaAdmin(admin.ModelAdmin):
+    actions = ['active_state','inactive_state']
     list_display = ('id','nombre','estado')
     ordering = ('id', 'nombre')
     search_fields = ('nombre', 'id','estado')
     list_editable = ('nombre',)
-    #list_display_links = ('nombre',)
-    list_filter= ('nombre',) 
-    list_per_page = 3 # Paginación
+    list_display_links = ('id',)
+    list_filter= ('nombre','estado') 
+    list_per_page = 5
     form = MarcaAdminForm
 
-    def get_actions (self,request):
+    @admin.action(description='Cambiar estado ACTIVO')
+    def active_state(self,request,queryset):
+        for marca in queryset:
+            marca.estado = 'ACTIVO'
+            marca.save()
+    
+    @admin.action(description='Cambiar estado INACTIVO')
+    def inactive_state(self,request,queryset):
+        for marca in queryset:
+            marca.estado = 'INACTIVO'
+            marca.save()
+
+    def get_actions(self, request):
         actions = super().get_actions(request)
-        if "delete_selected" in actions:
-            del actions["delete_selected"]
+        if 'delete_selected' in actions:
+            del actions['delete_selected']
         return actions
 
+    def has_delete_permission(self, request, obj=None):
+        return False
 
+admin.site.register(Marca,MarcaAdmin)
 
+# Genero Admin
+class GeneroAdminForm(forms.ModelForm):
+    estado = ChoiceField(choices=STATE_CHOICES)
+    
+    class Meta:
+        model = Genero
+        fields = ('descripcion','estado',)
+class GeneroAdmin(admin.ModelAdmin):
+    actions = ['active_state','inactive_state']
+    list_display = ('id','descripcion','estado')
+    ordering = ('id', 'descripcion')
+    search_fields = ('descripcion', 'id','estado')
+    list_editable = ('descripcion',)
+    list_display_links = ('id',)
+    list_filter= ('descripcion','estado') 
+    list_per_page = 5
+    form = GeneroAdminForm
+
+    @admin.action(description='Cambiar estado ACTIVO')
+    def active_state(self,request,queryset):
+        for genero in queryset:
+            genero.estado = 'ACTIVO'
+            genero.save()
+    
+    @admin.action(description='Cambiar estado INACTIVO')
+    def inactive_state(self,request,queryset):
+        for genero in queryset:
+            genero.estado = 'INACTIVO'
+            genero.save()
+
+    def get_actions(self, request):
+        actions = super().get_actions(request)
+        if 'delete_selected' in actions:
+            del actions['delete_selected']
+        return actions
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+admin.site.register(Genero,GeneroAdmin)
 
 # Register your models here.
-admin.site.register(Genero)
 admin.site.register(Sala)
 admin.site.register(Cargo)
 admin.site.register(Color)
 admin.site.register(Modelo)
-admin.site.register(Marca,MarcaAdmin)
 admin.site.register(Disponibilidad)
 admin.site.register(EstadoCivil)
 admin.site.register(DocIdentidad)
