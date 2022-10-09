65f882fd35c6f401ab1580dbee91c79f8131d720
Jorge Quintui <jorgealequinn@gmail.com>
2022-10-09 01:03:43 -0300

    Create: CALL PROCEDURE For BASE APP

---
+++ apps/base/stored_procedures.py
@@ -0,0 +1,9 @@
+# Importamos la librería para conectarnos a la base de datos
+from django.db import connections
+
+def genericDelete(db_user : str, pk : int, model_name : str):
+    with connections[db_user].cursor() as cursor:
+        outval = cursor.var(int).var
+        cursor.callproc("PKG_UTILS.SP_DELETE_ENTITY",[pk,model_name,outval])
+        print(outval.getvalue())        
+    return outval.getvalue()
