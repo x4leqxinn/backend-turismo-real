59fc876e10c716d6aa32bbbff71a48f221c7512d
Jorge Quintui <jorgealequinn@gmail.com>
2022-10-09 01:19:15 -0300

    Refact: genericDelete Stored Procedure

--- apps/base/stored_procedures.py
+++ apps/base/stored_procedures.py
@@ -4,6 +4,5 @@ from django.db import connections
 def genericDelete(db_user : str, pk : int, model_name : str):
     with connections[db_user].cursor() as cursor:
         outval = cursor.var(int).var
-        cursor.callproc("PKG_UTILS.SP_DELETE_ENTITY",[pk,model_name,outval])
-        print(outval.getvalue())        
+        cursor.callproc("PKG_UTILS.SP_DELETE_ENTITY",[pk,model_name,outval])        
     return outval.getvalue()
