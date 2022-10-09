e04472323a96e2fd8bf2d1a1c6114ea6b0043fd2
Jorge Quintui <jorgealequinn@gmail.com>
2022-10-09 02:30:52 -0300

    Update: ADD Booking Router

--- apps/business/api/routers.py
+++ apps/business/api/routers.py
@@ -1,4 +1,5 @@
 from rest_framework.routers import DefaultRouter
+from apps.business.api.bookings.bookings_views import BookingViewSet
 from apps.business.api.dwelling.dwelling_views import DwellingViewSet
 
 from apps.business.api.inventory.inventory_views import InventoryViewSet
@@ -7,5 +8,6 @@ router = DefaultRouter()
 
 router.register('inventory', InventoryViewSet, basename='inventory')
 router.register('dwelling', DwellingViewSet, basename='dwelling')
+router.register('booking',BookingViewSet, basename='booking')
 
 urlpatterns = router.urls
