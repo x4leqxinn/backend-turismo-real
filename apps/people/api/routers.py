25921de1a77103581d5333bad2f3b7c3b42c3e32
Jorge Quintui <jorgealequinn@gmail.com>
2022-10-09 21:13:13 -0300

    Refact: Client Routers

--- apps/people/api/routers.py
+++ apps/people/api/routers.py
@@ -1,9 +1,11 @@
 from rest_framework.routers import DefaultRouter
+from apps.people.api.client.client_views import ClientViewSet
 from apps.people.api.employee.employee_views import EmployeeViewSet
 
 router = DefaultRouter()
 
 router.register('employee',EmployeeViewSet, basename='employee')
+router.register('client',ClientViewSet, basename='client')
 
 urlpatterns = router.urls
 
