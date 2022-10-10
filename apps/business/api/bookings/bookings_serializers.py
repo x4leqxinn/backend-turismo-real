ec833c1c8fcc045966f5f17e3b4e9f572b923f39
Jorge Quintui <jorgealequinn@gmail.com>
2022-10-10 04:29:34 -0300

    Update: Client Booking OK :) :rage4:

--- apps/business/api/bookings/bookings_serializers.py
+++ apps/business/api/bookings/bookings_serializers.py
@@ -1,6 +1,6 @@
 from pyexpat import model
 from rest_framework import serializers
-from apps.base.models.db_models import Persona, Reserva
+from apps.base.models.db_models import Acompaniante, DocIdentidad, EstadoCivil, Genero, Persona, Reserva, CliAcom, Cliente, Vivienda
 from apps.locations.models import Cities
 
 
@@ -58,6 +58,7 @@ class BookingDatesSerializer(serializers.ModelSerializer):
 class BookingCreateSerializer(serializers.ModelSerializer):
 
     acompaniantes = serializers.ListField()
+
     class Meta:
         model = Reserva
         exclude = ('estado','creacion','actualizacion')
@@ -90,8 +91,55 @@ class BookingCreateSerializer(serializers.ModelSerializer):
         return values
 
     def create(self,validated_data):
+        acompaniantes = validated_data['acompaniantes'] 
 
-        print('OLA', validated_data['acompaniantes'])
-        
-        
+        print('ojo' , validated_data['id_viv'])
+
+        vivienda = Vivienda.objects.get(id = validated_data['id_viv'].id)
+        cliente = Cliente.objects.get(id = validated_data['id_cli'].id)
+
+        # Reserva
+        reserva = Reserva(id_cli = cliente, 
+        id_viv = vivienda, fecha_inicio = validated_data['fecha_inicio'],
+        fecha_termino = validated_data['fecha_termino'], abono = validated_data['abono'],
+        monto_pagado = validated_data['monto_pagado'], total_pago = validated_data['total_pago'],
+        cant_acompaniante = validated_data['cant_acompaniante'],
+        cant_total = validated_data['cant_total'])
+        reserva.save()
+
+        if len(acompaniantes) > 0:
+            # Agregamos los acompañantes al sistema
+            for index in range(len(acompaniantes)):  
+
+                docIdentidad = DocIdentidad.objects.get(id = acompaniantes[index]["id_doc"] )
+                estadoCivil = EstadoCivil.objects.get(id = acompaniantes[index]["id_est1"])
+                genero = Genero.objects.get(id = acompaniantes[index]["id_gen"])
+                persona = Persona(
+                    run = acompaniantes[index]["run"],
+                    dv = acompaniantes[index]["dv"],
+                    pasaporte = acompaniantes[index]["pasaporte"],
+                    nombre = acompaniantes[index]["nombre"],
+                    snombre = acompaniantes[index]["snombre"],
+                    ap_paterno = acompaniantes[index]["ap_paterno"],
+                    ap_materno = acompaniantes[index]["ap_materno"],
+                    fecha_nacimiento = acompaniantes[index]["fecha_nacimiento"],
+                    telefono = acompaniantes[index]["telefono"],
+                    num_calle = acompaniantes[index]["num_calle"],
+                    calle = acompaniantes[index]["calle"],
+                    id_ciu = acompaniantes[index]["id_ciu"],
+                    id_est = acompaniantes[index]["id_est"],
+                    id_pai = acompaniantes[index]["id_pai"],
+                    id_doc = docIdentidad,
+                    id_est1 = estadoCivil,
+                    id_gen = genero
+                )
+                persona.save()
+                
+                acompaniante = Acompaniante(id = persona)
+                acompaniante.save()
+
+                # Generar detalle
+                detalle = CliAcom(id_cli = cliente, id_aco = acompaniante, id_res = reserva)
+                detalle.save()
+
         return True
