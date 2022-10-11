1505eb6eed4bc59291bbd5c4aed94cfdaac2eb91
Jorge Quintui <jorgealequinn@gmail.com>
2022-10-11 03:00:40 -0300

    Update: Service Router

--- apps/business/api/routers.py
+++ apps/business/api/routers.py
@@ -1,13 +1,14 @@
 from rest_framework.routers import DefaultRouter
 from apps.business.api.bookings.bookings_views import BookingViewSet
 from apps.business.api.dwelling.dwelling_views import DwellingViewSet
-
 from apps.business.api.inventory.inventory_views import InventoryViewSet
+from apps.business.api.services.services_views import ServiceViewSet
 
 router = DefaultRouter()
 
 router.register('inventory', InventoryViewSet, basename='inventory')
 router.register('dwelling', DwellingViewSet, basename='dwelling')
 router.register('booking',BookingViewSet, basename='booking')
+router.register('service',ServiceViewSet, basename='service')
 
 urlpatterns = router.urls
