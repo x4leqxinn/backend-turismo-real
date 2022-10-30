19a30a06977b7b5fd5ac535917594a46b3202570
Jorge Quintui <jorgealequinn@gmail.com>
2022-10-30 03:55:02 -0300

    ADD: Generate Seed Command

---
+++ apps/base/management/commands/seed.py
@@ -0,0 +1,13 @@
+from django.core.management.base import BaseCommand, CommandError
+from apps.base.models.db_models import *
+from apps.base.utils.seed import *
+
+class Command(BaseCommand):
+    help = 'Comando que ejecuta un script para cargar la base de datos'
+
+    def handle(self, *args, **options):
+        print('Haciendo migraciones')
+        # TODO: 
+        # Realizar for con la logica de carga de los distintos modelos
+        run_seed()
+        print('OK')
