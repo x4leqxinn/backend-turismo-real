ddbbc87fc012f0baf7ca17a2dcc466ac559434cb
Jorge Quintui <jorgealequinn@gmail.com>
2022-11-13 21:14:57 -0300

    DELETE: Migrations

--- apps/base/models/base_model.py
+++ apps/base/models/base_model.py
@@ -4,8 +4,8 @@ from django.db import models
 class BaseModel(models.Model):
     id = models.AutoField(primary_key=True)
     estado = models.CharField(max_length=15, null=False, blank=False, default='ACTIVO')
-    creacion = models.DateTimeField('Fecha de Creación',auto_now=False, auto_now_add=True)
-    actualizacion = models.DateTimeField('Fecha de Actualización', auto_now=True, auto_now_add=False)
+    creacion = models.DateTimeField('Fecha de Creación',auto_now=False, auto_now_add=True, null=False)
+    actualizacion = models.DateTimeField('Fecha de Actualización', auto_now=True, auto_now_add=False, null=False)
 
     class Meta:
         abstract = True
