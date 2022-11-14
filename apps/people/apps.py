20c6222e6636bc4485c2c25f1fe909ec40229920
Jorge Quintui <jorgealequinn@gmail.com>
2022-11-13 21:54:57 -0300

    UPDATE: Verbose names application

--- apps/people/apps.py
+++ apps/people/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class PeopleConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.people'
+    verbose_name = 'Recursos Humanos'
