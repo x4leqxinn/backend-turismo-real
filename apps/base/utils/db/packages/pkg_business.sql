b24caa673f69e74a34d4fa5e703ab19c165b9507
Jorge Quintui <jorgealequinn@gmail.com>
2022-11-19 11:14:04 -0300

    ADD: PKG Dataframe procedure

--- apps/base/utils/db/packages/pkg_business.sql
+++ apps/base/utils/db/packages/pkg_business.sql
@@ -50,5 +50,77 @@ CREATE OR REPLACE PACKAGE BODY PKG_BUSINESS IS
         UPDATE CHECK_OUT SET total_multa = v_multa WHERE id = p_pk;
         COMMIT;
         
-     END SP_FINE_VALUE;
+    END SP_FINE_VALUE;
+
+    PROCEDURE SP_DF_ML IS 
+        -- ALL DEPARTMENTS
+        CURSOR C1 IS 
+        SELECT id FROM VIVIENDA;
+        -- DETAIL ROOM FOR DEPARTMENT
+        CURSOR C2(pk VIVIENDA.ID%TYPE) IS
+        SELECT
+        ds.id as "id_sala",
+        s.descripcion
+        FROM DETALLE_SALA ds
+        INNER JOIN SALA s
+        ON s.id = ds.id_sal
+        INNER JOIN INVENTARIO I
+        ON i.id = ds.id_inv
+        INNER JOIN VIVIENDA v
+        ON v.id = i.id_viv
+        WHERE i.id_viv = pk;
+        
+        -- DETAIL PRODUCTS FOR ROOMS
+        CURSOR C3(pk DETALLE_SALA.ID%TYPE) IS
+        SELECT p.descripcion
+        FROM detalle_producto dp
+        INNER JOIN PRODUCTO p
+        ON dp.id_pro = p.id
+        WHERE dp.id_det = pk;
+            
+        -- RECORD FOR CSV
+        r_csv dataframe%rowtype;
+
+    BEGIN
+
+        PKG_UTILS.SP_TRUNCATE_TABLE('DATAFRAME');
+        
+        FOR x in C1
+        LOOP
+            r_csv.id := x.id;
+            r_csv.rooms := NULL;
+            r_csv.products := NULL;
+            
+            -- 2 CURSOR
+            FOR y IN C2(x.id)
+            LOOP
+                BEGIN
+                    -- rooms
+                    r_csv.rooms := r_csv.rooms ||' '|| y.descripcion;
+                    DBMS_OUTPUT.PUT_LINE(y.descripcion);
+                    -- C3
+                    FOR z IN C3(y."id_sala")
+                    LOOP
+                        BEGIN
+                            r_csv.products := r_csv.products ||' '|| z.descripcion;
+                            DBMS_OUTPUT.PUT_LINE(z.descripcion);
+                        EXCEPTION
+                            WHEN OTHERS THEN
+                                DBMS_OUTPUT.PUT_LINE('No hay productos en esta sala');
+                        END;
+                    END LOOP;
+                EXCEPTION
+                    WHEN OTHERS THEN
+                        DBMS_OUTPUT.PUT_LINE('No hay sala');
+                END;
+            END LOOP;
+            
+            PKG_UTILS.v_sql := 'INSERT INTO DATAFRAME (id,rooms,products) VALUES (:id, :rooms, :products)';
+            EXECUTE IMMEDIATE PKG_UTILS.v_sql USING r_csv.id, nvl(LTRIM(r_csv.rooms),'N/A'), NVL(LTRIM(r_csv.products),'N/A');
+            COMMIT;
+            
+        END LOOP;
+        
+    END SP_DF_ML;
+
 END PKG_BUSINESS;
