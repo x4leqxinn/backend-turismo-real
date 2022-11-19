15f396b1402f342991e87619946b8e8a8e824123
Jorge Quintui <jorgealequinn@gmail.com>
2022-11-19 11:16:08 -0300

    ADD: Dataframe Model

--- apps/website/models.py
+++ apps/website/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Dataframe(models.Model):
+    id = models.IntegerField(null=False, primary_key=True)
+    rooms = models.TextField(blank=False)
+    products = models.TextField(blank=False, null=False)
+    class Meta:
+        db_table = 'dataframe'
+        verbose_name = "dataframe"
+        verbose_name_plural = "dataframe"
+        ordering = ['id']
