6f3e00d86f636336742895f3b45d73c6472cfbf2
Jorge Quintui <jorgealequinn@gmail.com>
2022-11-24 17:34:53 -0300

    ADD: Cuenta bancaria Migration

--- apps/business/models.py
+++ apps/business/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from apps.base.models.base_model import BaseModel
-
 # Create your models here.
 class CuentaBancaria(BaseModel):
     cvv = models.CharField(null=False, blank=False, max_length=3)
     fecha_expiracion = models.CharField(null=False, blank=False, max_length=4)
     nombre_titular = models.CharField(null=False, blank=False, max_length=200)
     numero_cuenta = models.CharField(null=False, blank=False, unique=True, max_length=100)
+    persona_id = models.OneToOneField('people.Persona', models.DO_NOTHING, db_column='persona_id')
+
     class Meta:
         managed = True
         db_table = "cuenta_bancaria"
