c1f9abf79d77fbc8d42eb1e0ceb8a8e793525975
Jorge Quintui <jorgealequinn@gmail.com>
2022-11-24 22:48:42 -0300

    ADD: Card router

--- apps/business/api/routers.py
+++ apps/business/api/routers.py
@@ -1,5 +1,5 @@
 from rest_framework.routers import DefaultRouter
-from apps.business.api.bookings.bookings_views import BookingViewSet
+from apps.business.api.bookings.bookings_views import BookingViewSet, CardViewSet
 from apps.business.api.dwelling.dwelling_views import DwellingViewSet
 from apps.business.api.inventory.inventory_views import InventoryViewSet
 from apps.business.api.services.services_views import ServiceViewSet
@@ -10,6 +10,7 @@ router = DefaultRouter()
 router.register('inventory', InventoryViewSet, basename='inventory')
 router.register('dwelling', DwellingViewSet, basename='dwelling')
 router.register('booking',BookingViewSet, basename='booking')
+router.register('card',CardViewSet, basename='card')
 router.register('service',ServiceViewSet, basename='service')
 router.register('shopping',ShoppingViewSet, basename='shopping')
 
