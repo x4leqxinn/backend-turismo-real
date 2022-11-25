d252ce134595e114e36e7289888ebe152ca6b437
Jorge Quintui <jorgealequinn@gmail.com>
2022-11-25 02:06:40 -0300

    ADD: System db router

---
+++ db_routers/system_router.py
@@ -0,0 +1,13 @@
+class SystemRouter:
+
+    def db_for_read(self, model, **hints):
+        return None
+
+    def db_for_write(self, model, **hints):
+        return 'system'
+
+    def allow_relation(self, obj1, obj2, **hints):
+        return None
+    
+    def allow_migrate(self, db, app_label, model_name=None, **hints):
+        return None
