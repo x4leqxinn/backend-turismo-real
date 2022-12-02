6d7d8541f3fa14f8b8c3c335e08fb38fa2f1e7da
Jorge Quintui <jorgealequinn@gmail.com>
2022-12-02 15:25:29 -0300

    UPDATE: Send Email Docstring

--- templates/emails/utils.py
+++ templates/emails/utils.py
@@ -2,35 +2,35 @@ from django.template.loader import get_template
 from django.core.mail import EmailMultiAlternatives
 from django.conf import settings
 
-from apps.base.models.db_models import Persona, Reserva
+from apps.base.models.db_models import Reserva
 
-def send_email(mail_to:str ,subject:str,booking: Reserva,page:str):
-    template = {
-        1 : 'emails/create_account/create-account.html',
-        2 : '',
-        3 : '',
-        4 : '',
-        5 : '',
-        6 : '',
+def send_email(mail_to:str,booking:Reserva,page:int):
+    """Send an email based on the reservation generated
+    Args: 
+        mail_to (str): recipient's email
+        Reserva (Reserva): booking data
+        page (int): Dict key
+        
+    Returns:
+        string (str): State of the process
+    """
+    option = {
+        1 : {'template':'emails/create_account/create-account.html','subject':'Bienvenido'},
+        2 : {'template':'emails/create_account/create-account.html','subject':'Bienvenido'},
+        3 : {'template':'emails/create_account/create-account.html','subject':'Bienvenido'},
+        4 : {'template':'emails/create_account/create-account.html','subject':'Bienvenido'},
+        5 : {'template':'emails/create_account/create-account.html','subject':'Bienvenido'},
+        6 : {'template':'emails/create_account/create-account.html','subject':'Bienvenido'},
     }.get(page)
 
-    if not template:
+    if not option:
         return f"The operation '{page}' is not supported!"
 
     context = {'booking': Reserva }
-    template = get_template(template)
+    template = get_template(option.template)
     content = template.render(context)
 
-    message = EmailMultiAlternatives(
-        # Asunto
-        subject,
-        settings.EMAIL_HOST_USER,
-        # Correo que hace los envios
-        settings.EMAIL_HOST_USER,
-        # Usuarios a los que se les envia el mail
-        [mail_to]
-        #cc=[] copia a 
-    )
+    message = EmailMultiAlternatives(option.subject,settings.EMAIL_HOST_USER,settings.EMAIL_HOST_USER,[mail_to])
 
     message.attach_alternative(content, 'text/html')
     message.send()
