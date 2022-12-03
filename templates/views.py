d0a64dd44d4faadfabed76c8a390c49d5c8889bf
Jorge Quintui <jorgealequinn@gmail.com>
2022-12-03 02:15:03 -0300

    ADD: Emails tests

--- templates/views.py
+++ templates/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.template.loader import get_template
 from django.core.mail import EmailMultiAlternatives
 from django.conf import settings
+from .emails.utils import prefix_decorator
+from apps.base.models.db_models import Reserva, Cliente
 
 def sendEmail(email):
     context = {'mail': "こんにちはホセ、私は私のロシアのバンカーから挨拶を送ります。" }
@@ -22,9 +24,13 @@ def sendEmail(email):
     message.attach_alternative(content, 'text/html')
     message.send()
 
+reserva = Reserva.objects.filter(id=1).first()
+
+@prefix_decorator(email_type='booking',page=1,booking=reserva)
 def index(request):
+    print('Hola')
     if request.method == 'POST':
         mail = request.POST.get('txtMail')
-        sendEmail(mail)
+        
 
     return render(request,'index.html',{}) 
