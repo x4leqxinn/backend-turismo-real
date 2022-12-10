fba79a96c3d02ec9e9748b3e479b82f4bb8a1ff4
Jorge Quintui <jorgealequinn@gmail.com>
2022-12-10 20:28:56 -0300

    DELETE: Refs

--- core/templates/views.py
+++ core/templates/views.py
@@ -3,10 +3,9 @@ from django.template.loader import get_template
 from django.core.mail import EmailMultiAlternatives
 from django.conf import settings
 from .emails.utils import prefix_decorator
-from apps.base.models.db_models import Reserva, CheckIn, CheckOut, Servicio, Compra
+from apps.base.models.db_models import Reserva
 from django.views.generic import CreateView, ListView, DeleteView, View
-from apps.users.models import User
-from datetime import datetime
+from core.templates.emails.utils import get_booking_dict
 
 
 def sendEmail(email):
@@ -28,7 +27,7 @@ def sendEmail(email):
     message.attach_alternative(content, 'text/html')
     message.send()
 
-reserva = Reserva.objects.filter(id=1).first()
+reserva = Reserva.objects.filter(id=2).first()
 
 @prefix_decorator(email_type='booking',page=1,booking=reserva)
 def index(request):
@@ -46,6 +45,7 @@ from django.template.loader import get_template
 from django.http import HttpResponse
 from xhtml2pdf import pisa
 from django.contrib.staticfiles import finders
+
 class BookingPdf(View):
 
     def link_callback(self,uri, rel):
@@ -84,7 +84,7 @@ class BookingPdf(View):
         #from datetime import date
         # Find the template and render it
         template = get_template('reports/booking.html')
-        context = get_booking_context(self.kwargs['pk'])
+        context = get_booking_dict(self.kwargs['pk'])
         html = template.render(context)
         # Create a django response object
         response = HttpResponse(content_type='application/pdf')
@@ -101,32 +101,3 @@ class BookingPdf(View):
 
 
 
-
-def get_booking_context(booking_id:int) -> dict:
-    booking = Reserva.objects.filter(id=booking_id).first()
-    client = booking.id_cli.id
-    checkin = CheckIn.objects.get(id_res=booking)
-    checkout = CheckOut.objects.get(id_res=booking)
-    checkout.total_multa = 0 if (checkout.total_multa is None) or (checkout.total_multa < 0) else checkout.total_multa
-    services = Servicio.objects.filter(id_reserva=booking.id)
-    user = User.objects.get(person=client)
-    dwelling = booking.id_viv
-    nights = (booking.fecha_termino - booking.fecha_inicio).days + 1
-    purchase = Compra.objects.get(id_reserva=booking)
-    paid_out = True if booking.total_pago == booking.monto_pagado else False
-    now = datetime.now()
-    context = {
-        'booking': booking,
-        'checkin': checkin,
-        'checkout': checkout,
-        'client': client,
-        'dwelling': dwelling,
-        'services': services,
-        'user': user,
-        'nights': nights,
-        'purchase': purchase,
-        'paid_out': paid_out,
-        'created_at': now,
-        'STATIC_ROOT': settings.STATIC_ROOT
-    }
-    return context
