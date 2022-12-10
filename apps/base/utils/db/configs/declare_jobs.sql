bfb872ada25f91f95435ad3ead16a2745b016fb6
Jorge Quintui <jorgealequinn@gmail.com>
2022-12-10 13:39:46 -0300

    ADD: JOB UPDATE SCORE

--- apps/base/utils/db/configs/declare_jobs.sql
+++ apps/base/utils/db/configs/declare_jobs.sql
@@ -7,4 +7,13 @@ BEGIN
     start_date          =>  NULL, 
     repeat_interval  => 'freq=monthly;bymonthday=1;byhour=01;byminute=01;bysecond=01', 
     enabled             =>  TRUE);
+
+    -- JOB QUE SE EJECUTA CADA MES Y AGREGA INFORMACIÓN AL DATAFRAME
+    dbms_scheduler.create_job (
+    job_name          =>  'JOB_SCORE', 
+    job_type            =>  'STORED_PROCEDURE',
+    job_action          =>  'PKG_BUSINESS.SP_UPDATE_SCORE', 
+    start_date          =>  NULL, 
+    repeat_interval  => 'freq=monthly;bymonthday=1;byhour=01;byminute=01;bysecond=01', 
+    enabled             =>  TRUE);
 END;
