a1ec5e03fc1d4f2bdd854dba990a9f59dbd96d1b
Jorge Quintui <jorgealequinn@gmail.com>
2022-12-14 23:57:44 -0300

    UPDATE: PAGADO TO COMPLETADO

--- apps/business/api/bookings/bookings_serializers.py
+++ apps/business/api/bookings/bookings_serializers.py
@@ -226,8 +226,8 @@ class CheckinSerializer(serializers.Serializer):
     estado = serializers.CharField(max_length=20)
 
     def validate_estado(self, value):
-        if value not in ('PENDIENTE','PAGADO','CANCELADO'):
-            raise serializers.ValidationError({'estado':'El estado debe ser PENDIENTE, PAGADO o CANCELADO.'})
+        if value not in ('PENDIENTE','COMPLETADO','CANCELADO'):
+            raise serializers.ValidationError({'estado':'El estado debe ser PENDIENTE, COMPLETADO o CANCELADO.'})
         return value
 
 class CheckoutSerializer(serializers.Serializer):
